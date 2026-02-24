d007932ef7b007242abb286b5fe4ae1689961b90
geloxh <ace.blazado7@gmail.com>
2026-02-24 15:59:04 +0800

    mongoose installation and User model

--- server/models/User.js
+++ server/models/User.js
@@ -1,1 +1,24 @@
-ECHO is on.
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+
+const userSchema = new mongoose.Schema({
+	firstName: { type: String, required: true },
+	lastName: { type: String, required: true},
+	email: { type: String, required: true, unique: true, lowercase: true },
+	password: { type: String, required: true }
+});
+
+userSchema.pre('save', async function(next) {
+	if (this.isModified('password')) {
+		this.password = await bcrypt.hash(this.password, 10 );
+	}
+	this.updatedAt = Date.now();
+	next();
+});
+
+userSchema.methods.comparePassword = async function(candidatePassword) {
+	return await bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
